e77616922655fa551227cb5cd971ea4e0238266d
unknown <hassaan9264@gmail.com>
2025-03-22 21:51:42 +0530

    admin page completed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allow any hostname
+        port: '',
+        pathname: '/**', // Allow any pathname
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
